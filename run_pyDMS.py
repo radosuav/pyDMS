a8d8a8045cda2fa7ff84546e71352adb5f771b34
Radoslaw Guzinski <radosuav@op.pl>
2018-08-21 11:58:05 +0200

    More style fixing.

--- run_pyDMS.py
+++ run_pyDMS.py
@@ -13,19 +13,19 @@ from pyDMS import DecisionTreeSharpener, NeuralNetworkSharpener
 from pyDMS import REG_sknn_ann, REG_sklearn_ann
 
 highResFilename = r""
-lowResFilename = r""    
-lowResMaskFilename = r"" 
-outputFilename = r""    
-    
+lowResFilename = r""
+lowResMaskFilename = r""
+outputFilename = r""
+
 ##########################################################################################
-                    
+
 if __name__ == "__main__":
 
     useDecisionTree = True
 
     commonOpts = {"highResFiles":               [highResFilename],
                   "lowResFiles":                [lowResFilename],
-                  "lowResQualityFiles":         [lowResMaskFilename], 
+                  "lowResQualityFiles":         [lowResMaskFilename],
                   "lowResGoodQualityFlags":     [255],
                   "cvHomogeneityThreshold":     0,
                   "movingWindowSize":           15,
@@ -33,11 +33,11 @@ if __name__ == "__main__":
     dtOpts =     {"perLeafLinearRegression":    True,
                   "linearRegressionExtrapolationRatio": 0.25}
     sknnOpts =   {'hidden_layer_sizes':         (10,),
-                  'activation':                 'tanh'} 
+                  'activation':                 'tanh'}
     nnOpts =     {"regressionType":             REG_sklearn_ann,
                   "regressorOpt":               sknnOpts}
 
-    start_time = time.time() 
+    start_time = time.time()
 
     if useDecisionTree:
         opts = commonOpts.copy()
@@ -47,32 +47,35 @@ if __name__ == "__main__":
         opts = commonOpts.copy()
         opts.update(nnOpts)
         disaggregator = NeuralNetworkSharpener(**opts)
-    
+
     print("Training regressor...")
     disaggregator.trainSharpener()
     print("Sharpening...")
     downscaledFile = disaggregator.applySharpener(highResFilename, lowResFilename)
     print("Residual analysis...")
-    residualImage, correctedImage = disaggregator.residualAnalysis(downscaledFile, lowResFilename, lowResMaskFilename, doCorrection = True)
+    residualImage, correctedImage = disaggregator.residualAnalysis(downscaledFile, lowResFilename,
+                                                                   lowResMaskFilename,
+                                                                   doCorrection=True)
     print("Saving output...")
-    highResFile = gdal.Open(highResFilename) 
-    if correctedImage is not None: 
+    highResFile = gdal.Open(highResFilename)
+    if correctedImage is not None:
         outImage = correctedImage
     else:
         outImage = downscaledFile
-    #outData = utils.binomialSmoother(outData)
-    outFile = utils.saveImg(outImage.GetRasterBand(1).ReadAsArray(), 
-                            outImage.GetGeoTransform(), 
-                            outImage.GetProjection(), 
+    # outData = utils.binomialSmoother(outData)
+    outFile = utils.saveImg(outImage.GetRasterBand(1).ReadAsArray(),
+                            outImage.GetGeoTransform(),
+                            outImage.GetProjection(),
                             outputFilename)
     residualFile = utils.saveImg(residualImage.GetRasterBand(1).ReadAsArray(),
                                  residualImage.GetGeoTransform(),
                                  residualImage.GetProjection(),
-                                 os.path.splitext(outputFilename)[0]+"_residual"+os.path.splitext(outputFilename)[1])
-    
+                                 os.path.splitext(outputFilename)[0] + "_residual" +
+                                 os.path.splitext(outputFilename)[1])
+
     outFile = None
     residualFile = None
     downsaceldFile = None
     highResFile = None
-        
+
     print(time.time() - start_time, "seconds")
